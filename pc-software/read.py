ea82641d8ab545d34d4d75c6a5322b794b3b8da4
Tuomas Nylund <tuomas.nylund@gmail.com>
2012-06-22 11:09:54 +0300

    Freq selection starting to look ok

--- pc-software/read.py
+++ pc-software/read.py
@@ -3,46 +3,41 @@
 import io
 
 from tkinter import *
-from tkinter import ttk
-
+#from tkinter import ttk
 from fgenio import Fgen
 
 
-#class FgenInterface(Frame):
-#    def __init__(self):
-#        Frame.__init__(self)
-#        self.master.title("Function Generator")
-#        self.fgenio = FgenIO()
-#        self.grid()
-
-
-class Segment(Frame):
-    def __init__(self,root,exp):
-        Frame.__init__(self)
+class FgenFselect(Frame):
+    def __init__(self,root):
+        Frame.__init__(self,bg="black")
         self.root = root
-
-        self.exp = exp
+        self.grid()
 
         self.var = IntVar()
         self.var.set(0)
 
-        #Button(self, command= lambda: root.change(self.exp,+1)).pack()
-        Button(self).pack()
-        self.num = ttk.Label(self,textvariable=self.var).pack()
-        Button(self).pack()
-        #Button(self, command= lambda: root.change(self.exp,-1)).pack()
+        self.num = []
+
+        for i in range(0,10):
+            Button(self, relief="flat",highlightbackground="black",bg="black", command= lambda i=i: self.btnCallback(i,1)).grid(column=i,row=0)
+            self.num.append(Label(self,textvariable=self.var,bg="black",fg="white").grid(column=i,row=1))
+            Button(self, relief="flat",highlightbackground="black",bg="black", command= lambda i=i: self.btnCallback(i,-1)).grid(column=i,row=2)
+
+    def btnCallback(self,exp,change):
+        print (change)
+        print (exp)
+
 
     def set(val):
         self.var = val
 
-class Digits(Frame):
-    def __init__(self,root):
-        Frame.__init__(self)
-        self.root = root
-
-        self.grid()
-
-        Segment(self,0).grid(column=0,row=0)
+    #def __init__(self,root):
+    #    Frame.__init__(self)
+    #    self.root = root
+#
+#        self.grid()
+#
+#        Segment(self,0).grid(column=0,row=0)
         #Segment(self,0).grid(column=1,row=0)
         #Segment(self,0).grid(column=2,row=0)
         #Segment(self,0).grid(column=3,row=0)
@@ -51,44 +46,6 @@ class Digits(Frame):
     #    print ("omg")
 
 
-class FgenFselect(Frame):
-    def __init__(self,root,name):
-        Frame.__init__(self)
-        self.root = root
-        self.grid()
-
-        #variables
-        self.frange = StringVar()
-        self.frange.set("Hz")
-        self.fval = DoubleVar()
-
-        #label
-        ttk.Label(self,text=name).grid(column=0,row=0)
-
-        #scale
-        self.scale = Scale(self, variable=self.fval, from_=0, to=1000,showvalue=0, resolution=0.1,orient=HORIZONTAL)
-        self.scale.grid(column=1,row=0)
-        self.scale.bind(sequence='<4>',func=self.Mouse)
-        self.scale.bind(sequence='<5>',func=self.Mouse)
-
-        #entry
-        self.entry = ttk.Entry(self,textvariable=self.fval,width=6).grid(column=2,row=0)
-
-        #menu
-        self.menu = OptionMenu(self,self.frange,"Hz","kHz","MHz", command=self.RangeChange).grid(column=3,row=0)
-
-    def RangeChange(self,event):
-        print (self.frange.get())
-
-    def Mouse(self,event):
-        if event.type == '4':
-            if event.num == 4:
-                self.fval.set(self.fval.get()+0.1)
-                print("scrollup!")
-            elif event.num == 5:
-                self.fval.set(self.fval.get()-0.1)
-                print("scrolldown!")
-
         
         
 #fgen = Fgen()
@@ -96,16 +53,7 @@ class FgenFselect(Frame):
 
 root = Tk()
 
-digits = Digits(root)
-digits.pack()
+fsel = FgenFselect(root)
+fsel.pack()
 
 root.mainloop()
-
-
-
-#fsel1 = FgenFselect(root,"f1")
-#fsel1.pack()
-
-#fsel2 = FgenFselect(root,"f2")
-#fsel2.pack()
-
